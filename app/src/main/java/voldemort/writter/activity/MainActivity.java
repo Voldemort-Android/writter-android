6524658e498d147d61b387a447b708db9d04d0a2
Unknown <alvinquach91@gmail.com>
2018-12-09 10:13:17 -0800

    created android project with login activity and blank main activity

---
+++ app/src/main/java/voldemort/writter/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package voldemort.writter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import voldemort.writter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
