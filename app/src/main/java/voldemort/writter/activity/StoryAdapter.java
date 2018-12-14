fe53b06e156b1ffb603ff9fe163b79135f620bec
Safa Mahbub <safamahbub357@gmail.com>
2018-12-13 23:01:29 -0800

    adapter to activity

--- app/src/main/java/voldemort/writter/activity/StoryAdapter.java
+++ app/src/main/java/voldemort/writter/activity/StoryAdapter.java
@@ -3,10 +3,13 @@ package voldemort.writter.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+
 
 import voldemort.writter.R;
 import voldemort.writter.data.model.Story;
@@ -46,6 +49,11 @@ public class StoryAdapter  extends RecyclerView.Adapter<StoryAdapter.StoryHolder
     public class StoryHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView title;
         TextView username;
+        String sendTitle = "";
+        String sendAuthor = "";
+        String sendText = "";
+        int sendRank = 0;
+
 
         public StoryHolder(View itemView) {
             super(itemView);
@@ -61,12 +69,27 @@ public class StoryAdapter  extends RecyclerView.Adapter<StoryAdapter.StoryHolder
             usernameText += " • views: ";
             usernameText += mStories.get(listIndex).getViews();
             username.setText(usernameText);
+
+            sendTitle = mStories.get(listIndex).getTitle();
+            sendAuthor = mStories.get(listIndex).getAuthor().getFirstName();
+            sendText = mStories.get(listIndex).getText();
+//            sendRank = mStories.get(listIndex).getAuthor().;
+
             itemView.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-//            String urlString = mStories.get(getAdapterPosition()).getUrl();
+            Intent intent = new Intent(mContext, StoryActivity.class);
+            intent.putExtra("Title", mStories.get(getAdapterPosition()).getTitle());
+//            intent.putExtra("Author", mStories.get(getAdapterPosition()).getAuthor().getFirstName());
+//            intent.putExtra("Text", mStories.get(getAdapterPosition()).getText());
+//            Log.d("check title", intent.getStringExtra("Title"));
+//            Log.d("check author", intent.getStringExtra("Author"));
+//            Log.d("check text", intent.getStringExtra("Text"));
+
+            mContext.startActivity(intent);
+//            String urlString = mStories.get(getAdapterPosition()).getText();
 //            Intent intent = new Intent(mContext, WebActivity.class);
 //            intent.putExtra("urlString", urlString);
 //            mContext.startActivity(intent);
